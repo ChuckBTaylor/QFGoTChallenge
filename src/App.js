5c24c6bafc1f23c271edd9da0ed3f7fbae0057d0
ChuckBTaylor <WLScripps@gmail.com>
2019-03-26 08:22:37 -0500

    Playing with scss file

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './assets/stylesheets/app.css';
+import './assets/stylesheets/app.scss';
 
 class App extends Component {
   render() {
