29f53017095d4502e8d08226dbef0db8af1befb1
ChuckBTaylor <WLScripps@gmail.com>
2019-03-25 23:07:21 -0500

    Adding services file

---
+++ src/services/apiRequests.js
@@ -0,0 +1,25 @@
+
+export default function () {
+  const API_ROOT = "https://www.anapioficeandfire.com/api/";
+  const requestInfo = {
+    method: 'GET',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json'
+    }
+  }
+
+  return {
+    books: {
+
+    },
+
+    characters: {
+
+    },
+
+    houses: {
+
+    }
+  }
+}
