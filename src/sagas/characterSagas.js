19b5776de2c360e335ec26029a57793ff1263818
ChuckBTaylor <WLScripps@gmail.com>
2019-03-26 23:03:57 -0500

    updating character sagas

--- src/sagas/characterSagas.js
+++ src/sagas/characterSagas.js
@@ -3,7 +3,7 @@ import apiRequests  from '../services/apiRequests';
 import { characterActions } from '../constants/constants';
 import { call, put } from 'redux-saga/effects';
 
-export function* fetchCharacters() {
+function* fetchCharacters() {
   try {
     const data = yield call(apiRequests().characters.fetchAll);
     console.log(data);    
